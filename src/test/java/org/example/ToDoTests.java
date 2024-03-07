6213a9efc758d13b34ce19be5cc11cda2ea1d4c7
Oleksandr Tsupko <oleksandr.tsupko@gmail.com>
2024-03-07 11:19:14 +0200

    ToDoTests.java modified

--- src/test/java/org/example/ToDoTests.java
+++ src/test/java/org/example/ToDoTests.java
@@ -5,9 +5,14 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -17,6 +22,23 @@ public class ToDoTests {
     private WebDriver driver;
     private WebDriverWait webDriverWait;
     private Actions actions;
+
+    private WebElement waitAndFindElement(By locator) {
+        return webDriverWait.until(
+                ExpectedConditions.presenceOfElementLocated(locator));
+    }
+
+    private void openTechnologyApp(String technologyName) {
+        var technologyLink = waitAndFindElement(By.cssSelector(technologyName));
+        technologyLink.click();
+    }
+
+    private void addNewToDoItem(String itemText) {
+        var toDoInput = waitAndFindElement(
+                By.xpath("//input[@placeholder='What needs to be done?']"));
+        toDoInput.sendKeys(itemText);
+        actions.click(toDoInput).sendKeys(Keys.ENTER).pause(1000).perform();
+    }
 
     @BeforeAll
     public static void setUpClass() {
@@ -34,6 +56,8 @@ public class ToDoTests {
     @Test
     public void verifyToDoListCreatedSuccessfully() {
         driver.navigate().to("https://todomvc.com");
+        openTechnologyApp("[data-source='http://backbonejs.org/']");
+        addNewToDoItem("ToDo Text");
     }
 
     @AfterEach
